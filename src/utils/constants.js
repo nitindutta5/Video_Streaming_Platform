6c3b7aeb94710302d3024d9eba74f0ab6a6f1805
nitindutta5 <nitindutta5@gmail.com>
2023-04-02 19:08:24 +0530

    Removed api key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-const GOOGLE_API_KEY = "AIzaSyAGnxJbJttH8tsXuo6UpuDL3T7Oc2OiAls"
+const GOOGLE_API_KEY = process.env.REACT_APP_API_KEY; 
 export const YOUTUBE_API = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key='+ GOOGLE_API_KEY;
